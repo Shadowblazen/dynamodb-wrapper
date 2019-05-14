611d80d5fa9dc76093d43a05c659db4a3c483d03
Jeff Bradford <jeff.bradford@apexlearning.com>
2019-05-13 23:12:40 -0700

    update devDependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,46 @@
 // npm modules
-var del = require('del');
-var gulp = require('gulp');
-var tslint = require('gulp-tslint');
-var tsc = require('gulp-typescript');
-var jasmine = require('gulp-jasmine');
-var JasmineConsoleReporter = require('jasmine-console-reporter');
-var runSequence = require('run-sequence');
-var istanbul = require('gulp-istanbul');
+const del = require('del');
+const { src, dest, series } = require('gulp');
+const tslint = require('gulp-tslint');
+const tsc = require('gulp-typescript');
+const jasmine = require('gulp-jasmine');
+const JasmineConsoleReporter = require('jasmine-console-reporter');
+const istanbul = require('gulp-istanbul');
 
-/*********************************************************************************************************************
- * Public tasks: run these from the console, e.g. 'gulp start' or 'gulp test'
- *********************************************************************************************************************/
-
-gulp.task('default', function (done) {
-    runSequence(
-        'lint',
-        'build',
-        'test.coverage',
-        done
-    );
-});
-
-gulp.task('start', ['build', 'lint', 'watch.lib']);
-
-gulp.task('test', function () {
-    var options = {
-        reporter: new JasmineConsoleReporter({
-            colors: process.argv.indexOf('--no-color') === -1,
-            verbosity: 3
-        })
-    };
-
-    return gulp.src('bin/**/*spec.js').pipe(jasmine(options));
-});
-
-gulp.task('test.coverage', function (done) {
-    runSequence(
-        'pretest.coverage.instrumentation',
-        'test',
-        'posttest.coverage.reports',
-        done
-    );
-});
-
-/*********************************************************************************************************************
- * Create Build Package
- *********************************************************************************************************************/
-
-gulp.task('build', function (done) {
-    runSequence(
-        'clean',
-        ['transpile.lib', 'transpile.test'],
-        done
-    );
-});
-
-gulp.task('transpile.lib', function () {
-    var tsProject = tsc.createProject('tsconfig.json');
-
-    return gulp.src('lib/**/*.ts')
-        .pipe(tsProject())
-        .pipe(gulp.dest('bin'));
-});
-
-gulp.task('transpile.test', function () {
-    var tsProject = tsc.createProject('tsconfig.json');
-
-    return gulp.src('test/**/*.ts')
-        .pipe(tsProject())
-        .pipe(gulp.dest('test'));
-});
-
-gulp.task('clean', function (done) {
+function cleanTask(done) {
     del(['bin', 'coverage', 'test/**/*.js']).then(function () {
         done();
     });
-});
+}
 
-gulp.task('prepublish', function (done) {
-    del(['bin/jasmine-runner.js', 'bin/**/*.spec.js']).then(function () {
-        done();
-    });
-});
+function lintTask() {
+    return src('lib/**/*.ts')
+        .pipe(tslint({
+            formatter: 'verbose'
+        }))
+        .pipe(tslint.report({
+            emitError: true
+        }));
+}
 
-/*********************************************************************************************************************
- * Linting, Testing, Precommit-hooks, etc.
- *********************************************************************************************************************/
+function transpileLibTask() {
+    const tsProject = tsc.createProject('tsconfig.json');
 
-gulp.task('pretest.coverage.instrumentation', function () {
-    var filesGlob = [
+    return src('lib/**/*.ts')
+        .pipe(tsProject())
+        .pipe(dest('bin'));
+}
+
+function transpileTestTask() {
+    const tsProject = tsc.createProject('tsconfig.json');
+
+    return src('test/**/*.ts')
+        .pipe(tsProject())
+        .pipe(dest('test'));
+}
+
+function pretestCoverageInstrumentationTask() {
+    const filesGlob = [
         'bin/**/*.js',
 
         // ignore test-related files
@@ -96,40 +48,34 @@ gulp.task('pretest.coverage.instrumentation', function () {
         '!bin/jasmine-runner.js'
     ];
 
-    return gulp.src(filesGlob)
+    return src(filesGlob)
         .pipe(istanbul({
             includeUntested: true
         }))
         .pipe(istanbul.hookRequire());
-});
-
-gulp.task('posttest.coverage.reports', function () {
-    return gulp.src('bin/**/*.spec.js')
-        .pipe(istanbul.writeReports());
-});
-
-gulp.task('lint.noerror', function () {
-    return tslintHelper('lib/**/*.ts', false);
-});
-
-gulp.task('lint', function () {
-    return tslintHelper('lib/**/*.ts', true);
-});
-
-gulp.task('watch.lib', function () {
-    gulp.watch(['lib/**/*.ts'], ['build', 'lint.noerror']);
-});
-
-/*********************************************************************************************************************
- * Helper methods
- *********************************************************************************************************************/
-
-function tslintHelper(glob, emitError) {
-    return gulp.src(glob)
-        .pipe(tslint({
-            formatter: 'verbose'
-        }))
-        .pipe(tslint.report({
-            emitError: emitError
-        }));
 }
+
+function posttestCoverageReportsTask() {
+    return src('bin/**/*.spec.js')
+        .pipe(istanbul.writeReports());
+}
+
+function testTask() {
+    return src('bin/**/*spec.js').pipe(jasmine({
+        reporter: new JasmineConsoleReporter({
+            colors: process.argv.indexOf('--no-color') === -1,
+            verbosity: 3
+        })
+    }));
+}
+
+function prepublishTask(done) {
+    del(['bin/jasmine-runner.js', 'bin/**/*.spec.js']).then(function () {
+        done();
+    });
+}
+
+exports.prepublish = prepublishTask;
+exports.build = series(lintTask, cleanTask, transpileLibTask, transpileTestTask);
+exports.test = series(exports.build, pretestCoverageInstrumentationTask, testTask, posttestCoverageReportsTask);
+exports.default = exports.test;
